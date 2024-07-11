bd6a67fa09c161df8a2b47653fd5fef11670d46f
Mohd Ayan haque <ayanhaque904@gmail.com>
2024-07-11 20:02:58 +0530

    small change

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -61,12 +61,11 @@ export default function Page() {
         </a>
 
         <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-          Welcome to Squid 🦑
+        Welcome to PrepGenie 🧞
         </h1>
 
         <p className="mt-4 leading-relaxed text-gray-500">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-          quibusdam aperiam voluptatum.
+        Your AI-Powered Interview Coach. Prepare, Practice, and Perfect Your Skills to Ace Every Question, Every Time!
         </p>
       </div>
 
